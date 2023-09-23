699ae3ade8f2562c9282d9adc15ff92fd7ebe169
niqzart <qwert45hi@yandex.ru>
2023-09-23 05:59:45 +0300

    feat: mini-logout tests

---
+++ src/main/java/niqzart/lab3/member/LogoutPage.java
@@ -0,0 +1,22 @@
+package niqzart.lab3.member;
+
+import niqzart.lab3.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LogoutPage extends BasePage {
+  @Override
+  protected String getPath() {
+    return "/users/logout";
+  }
+
+  public LogoutPage(WebDriver driver) {
+    super(driver);
+  }
+
+  private static final String logoutButtonXPath = "//form[@action='/users/logout']//button[contains(text(), 'Log out')]";
+
+  public void clickLogoutButton() {
+    driver.findElement(By.xpath(logoutButtonXPath)).click();
+  }
+}
---
+++ src/test/java/niqzart/lab3/member/LogoutPageTest.java
@@ -0,0 +1,31 @@
+package niqzart.lab3.member;
+
+import niqzart.lab3.BaseTest;
+import niqzart.lab3.LoginPage;
+import niqzart.lab3.Utils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LogoutPageTest extends BaseTest {
+  @Test
+  void testAuthorizedLogout() {
+    drivers.forEach(driver -> {
+      new LoginPage(driver).login();
+
+      LogoutPage logoutPage = new LogoutPage(driver);
+
+      logoutPage.clickLogoutButton();
+
+      assertEquals(driver.getCurrentUrl(), Utils.formatUrl("/"));
+    });
+  }
+
+  @Test
+  void testUnauthorizedLogout() {
+    drivers.forEach(driver -> {
+      new LogoutPage(driver);
+      assertEquals(driver.getCurrentUrl(), Utils.formatUrl("/"));
+    });
+  }
+}
